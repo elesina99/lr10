420f1c1c0be6eef29c3f24de02748c6ec5ba33bd
elesina99.github.io <elesina99@gmail.com>
2025-10-21 00:03:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,17 @@ const fruitImages = [
 
 let attempt = 0; 
 const maxAttempts = 3;
-
 let userName = "";
 
 window.onload = () => {
   do {
-    userName = prompt("Введіть ваше ім’я:");
+    userName = prompt("Р’РІРµРґС–С‚СЊ РІР°С€Рµ С–РјвЂ™СЏ:");
     if (userName === null) {
-      alert("Введіть ім’я, щоб почати гру!");
+      alert("Р’РІРµРґС–С‚СЊ С–РјвЂ™СЏ, С‰РѕР± РїРѕС‡Р°С‚Рё РіСЂСѓ!");
     } else {
       userName = userName.trim(); 
       if (userName === "") {
-        alert("Не може бути порожнім!");
+        alert("РќРµ РјРѕР¶Рµ Р±СѓС‚Рё РїРѕСЂРѕР¶РЅС–Рј!");
       }
     }
   } while (!userName);
@@ -28,8 +27,8 @@ window.onload = () => {
   const attemptText = document.getElementById("attemptText");
   const message = document.getElementById("message");
 
-  message.textContent = `Гравець: ${userName}`;
-  attemptText.textContent = `Спроба ${attempt} з ${maxAttempts}`;
+  message.textContent = `Р“СЂР°РІРµС†СЊ: ${userName}`;
+  attemptText.textContent = `РЎРїСЂРѕР±Р° ${attempt} Р· ${maxAttempts}`;
 
   btn.addEventListener("click", generateSlots);
 
@@ -39,29 +38,23 @@ window.onload = () => {
       document.getElementById("col2"),
       document.getElementById("col3")
     ];
-
     columns.forEach(col => col.innerHTML = "");
+    let matrix = [[], [], []];
 
-    let matrix = [];
+    for (let col = 0; col < 3; col++) {
+      let used = new Set(); 
 
-    for (let i = 0; i < 3; i++) {
-      let used = new Set();
-      let colItems = [];
-
-      for (let j = 0; j < 3; j++) {
+      for (let row = 0; row < 3; row++) {
         let index;
         do {
           index = Math.floor(Math.random() * fruitImages.length);
         } while (used.has(index));
         used.add(index);
-        colItems.push(index);
-
         const img = document.createElement("img");
         img.src = fruitImages[index];
-        columns[i].appendChild(img);
+        columns[col].appendChild(img);
+        matrix[row][col] = index;
       }
-
-      matrix.push(colItems);
     }
 
     checkWin(matrix);
@@ -73,27 +66,26 @@ window.onload = () => {
     const message = document.getElementById("message");
 
     let win = false;
-
     for (let row = 0; row < 3; row++) {
-      const first = matrix[0][row];
-      if (matrix[1][row] === first && matrix[2][row] === first) {
+      const first = matrix[row][0];
+      if (matrix[row][1] === first && matrix[row][2] === first) {
         win = true;
         break;
       }
     }
 
     if (win) {
-      message.textContent = `Вітаю, ${userName}! Ви виграли!`;
-	  btn.disabled = true;
+      message.textContent = `Р’С–С‚Р°СЋ, ${userName}! Р’Рё РІРёРіСЂР°Р»Рё!`;
+      btn.disabled = true;
       btn.style.backgroundColor = "gray";
     } else if (attempt < maxAttempts - 1) { 
       attempt++;
-      attemptText.textContent = `Спроба ${attempt} з ${maxAttempts}`;
-      message.textContent = `На жаль, але ні, ${userName}.`;
+      attemptText.textContent = `РЎРїСЂРѕР±Р° ${attempt} Р· ${maxAttempts}`;
+      message.textContent = `РќР° Р¶Р°Р»СЊ, ${userName}, СЃРїСЂРѕР±СѓР№С‚Рµ С‰Рµ СЂР°Р·.`;
     } else {
       attempt = maxAttempts;
-      attemptText.textContent = `Спроба ${attempt} з ${maxAttempts}`;
-      message.textContent = ` ${userName}, спроби закінчились. Ви програли.`;
+      attemptText.textContent = `РЎРїСЂРѕР±Р° ${attempt} Р· ${maxAttempts}`;
+      message.textContent = `${userName}, СЃРїСЂРѕР±Рё Р·Р°РєС–РЅС‡РёР»РёСЃСЊ. Р’Рё РїСЂРѕРіСЂР°Р»Рё.`;
       btn.disabled = true;
       btn.style.backgroundColor = "gray";
     }
